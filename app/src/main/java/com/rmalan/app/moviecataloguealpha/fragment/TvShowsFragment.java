54b7909dd75244efd67d149c80e1c5de280d32c1
M. Raka Maulana A <m.rakamaulanaalan@studentpartner.com>
2019-09-02 11:19:59 +0700

    Initial build - bottom navigation & fragment

---
+++ app/src/main/java/com/rmalan/app/moviecataloguealpha/fragment/TvShowsFragment.java
@@ -0,0 +1,19 @@
+package com.rmalan.app.moviecataloguealpha.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.rmalan.app.moviecataloguealpha.R;
+
+public class TvShowsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tv_shows, container, false);
+    }
+
+}
